1429c7ede8e18aa8b57d88f2b038a61b8a488450
Jonathan Montoya <montoyajdev@gmail.com>
2022-08-06 18:41:59 -0500

    Import ReactMarkdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from 'react-markdown';
+
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
 
@@ -13,9 +15,9 @@ function PostContent() {
 	const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
 	return (
-		<article>
+		<article className={classes.content}>
 			<PostHeader title={DUMMY_POST.title} image={imagePath} />
-			CONTENT
+			<ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
 		</article>
 	);
 }
