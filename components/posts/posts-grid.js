088770dcc38fdc099595ad4dbd6fd34546f3e263
Jonathan Montoya <montoyajdev@gmail.com>
2022-08-05 23:42:16 -0500

    Render Dummy Post Data

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -1,15 +1,16 @@
 import PostItem from './post-item';
 import classes from './posts-grid.module.css';
-import { v4 as uuid } from 'uuid';
 
-export default function PostsGrid(props) {
+function PostsGrid(props) {
 	const { posts } = props;
 
 	return (
-		<ul className="classes.grid">
-			{posts.map((post) => (
-				<PostItem key={uuid()} />
+		<ul className={classes.grid}>
+			{posts?.map((post) => (
+				<PostItem key={post.slug} post={post} />
 			))}
 		</ul>
 	);
 }
+
+export default PostsGrid;
